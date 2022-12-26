d0e090ac9c63e925dba3fcb673800778ee4c73a7
Xavier Poon <xavierpoon737@gmail.com>
2022-12-26 15:35:39 +1100

    change from yarn to npm in procfile

---
+++ src/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+function initApp() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const app = (0, express_1.default)();
+        app.use(express_1.default.json());
+        app.get("/", (req, res) => {
+            res.send("hello world - aws-server-eg");
+        });
+        return app;
+    });
+}
+function startup() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const app = yield initApp();
+        const server = http_1.default.createServer(app);
+        server.listen("3000", () => console.log(`Server listening on port 3000`));
+    });
+}
+startup();
