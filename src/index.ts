f97f5544c17aa3d29a22f1a8457ecf621fe9dde3
Xavier Poon <xavierpoon737@gmail.com>
2022-12-26 16:10:15 +1100

    change port to 8080

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ async function initApp() {
 async function startup() {
   const app = await initApp();
   const server = http.createServer(app);
-  server.listen("3000", () =>
-    console.log(`Server listening on port 3000`),
+  server.listen("8080", () =>
+    console.log(`Server listening on port 8080`),
   );
 }
 
